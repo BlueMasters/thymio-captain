9be32b0c812205bdfd7e9ac7b6f231247379f0ce
Lucy Linder <lucy.derlin@gmail.com>
2016-02-22 18:32:17 +0100

    fix cookie/header aut

--- frontend/webapp/ajs/rest/rest.module.js
+++ frontend/webapp/ajs/rest/rest.module.js
@@ -18,35 +18,41 @@
      */
     angular
         .module( 'thymioCaptain.rest', ['ngResource', 'base64', 'ngCookies'] )
-        .config( function( $httpProvider, $base64 ){
+        .run( addAuth )
+        .config( configure );
 
-            // handle authentication cookie
-            // note: service can't be injected directly into config, so I do it manually
-            // more info at: http://stackoverflow.com/questions/15358029/why-am-i-unable-to-inject-angular-cookies
-            angular.injector( ['ngCookies'] ).invoke( ['$cookies', function( $cookies ){
-                var auth = $cookies.get( "session-key" );
-                if( auth ){
-                    $httpProvider.defaults.header['Authorization'] = "Cookie " + auth;
-                }
-            }] );
+    // ----------------------------------------------------
+
+    function addAuth( $http, $cookies ){
+        // $cookies is a service, hence not accessible from config
+        // while $httpProvider is ONLY accessible from config.
+        // So here is a workaround
+        var auth = $cookies.get( "session-key" );
+        if( auth ){
+            console.log( auth );
+            $http.defaults.headers.common['Authorization'] = "Cookie " + auth;
+        }
+    }
 
 
-            // handle the from/to base64 (program argument only)
-            $httpProvider.defaults.transformRequest.unshift( function( data, headerGetter ){
-                if( data && data.program ){
-                    data.program = $base64.encode( JSON.stringify( data.program ) );
-                }
-                return data;
-            } );
+    function configure( $httpProvider, $base64 ){
 
-            $httpProvider.defaults.transformResponse.push( function( data, headerGetter ){
-                if( data && data.program ){
-                    data.program = JSON.parse( $base64.decode( data.program ) );
-                }
-                return data;
-            } );
-
+        // handle the from/to base64 (program argument only)
+        $httpProvider.defaults.transformRequest.unshift( function( data, headerGetter ){
+            if( data && data.program ){
+                data.program = $base64.encode( JSON.stringify( data.program ) );
+            }
+            return data;
         } );
+
+        $httpProvider.defaults.transformResponse.push( function( data, headerGetter ){
+            if( data && data.program ){
+                data.program = JSON.parse( $base64.decode( data.program ) );
+            }
+            return data;
+        } );
+
+    }
 
 
 }());
