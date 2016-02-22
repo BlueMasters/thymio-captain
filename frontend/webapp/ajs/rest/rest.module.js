622ef950ece712f577b3a0dfc35bb66fd42f7393
Lucy Linder <lucy.derlin@gmail.com>
2016-02-22 17:38:32 +0100

    handle auth header

--- frontend/webapp/ajs/rest/rest.module.js
+++ frontend/webapp/ajs/rest/rest.module.js
@@ -18,14 +18,18 @@
      */
     angular
         .module( 'thymioCaptain.rest', ['ngResource', 'base64', 'ngCookies'] )
-        .config( function( $httpProvider, $base64, $cookies ){
+        .config( function( $httpProvider, $base64 ){
 
             // handle authentication cookie
-            var auth = $cookies.get("session-key");
+            // note: service can't be injected directly into config, so I do it manually
+            // more info at: http://stackoverflow.com/questions/15358029/why-am-i-unable-to-inject-angular-cookies
+            angular.injector( ['ngCookies'] ).invoke( ['$cookies', function( $cookies ){
+                var auth = $cookies.get( "session-key" );
+                if( auth ){
+                    $httpProvider.defaults.header['Authorization'] = "Cookie " + auth;
+                }
+            }] );
 
-            if(auth){
-                $httpProvider.defaults.header['Authorization'] = "Cookie " + auth;
-            }
 
             // handle the from/to base64 (program argument only)
             $httpProvider.defaults.transformRequest.unshift( function( data, headerGetter ){
