84ed3ae7f104a8f59176b6571da81abe8b4812bc
Jacques Supcik <jacques.supcik@hefr.ch>
2016-02-24 15:12:49 +0100

    handle empty program

--- frontend/webapp/ajs/rest/rest.module.js
+++ frontend/webapp/ajs/rest/rest.module.js
@@ -46,8 +46,12 @@
         } );
 
         $httpProvider.defaults.transformResponse.push( function( data, headerGetter ){
-            if( data && data.program ){
-                data.program = JSON.parse( $base64.decode( data.program ) );
+            if( data && data.hasOwnProperty("program") ){
+                if(data.program) {
+                    data.program = JSON.parse($base64.decode(data.program));
+                }else{
+                    data.program = [];
+                }
             }
             return data;
         } );
