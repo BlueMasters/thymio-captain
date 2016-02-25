43d67fa9da6fca8e08882410cfd978a7b0537e71
Jacques Supcik <jacques.supcik@hefr.ch>
2016-02-25 16:48:54 +0100

    WIP

--- cards/print_admin_url.sh
+++ cards/print_admin_url.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 URL="https://thymio.tk/cardlogin"
+source secret.sh
+SECRET=$ADMIN_SECRET
+
 for i in $(seq ${1:-10}); do
     x=$(../genid/genid -short -key "$SECRET")
     echo "$URL/$x"
--- cards/print_user_url.sh
+++ cards/print_user_url.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 URL="https://thymio.tk/start"
+source secret.sh
+SECRET=$USER_SECRET
+
 for i in $(seq ${1:-10}); do
     x=$(../genid/genid -short -key "$SECRET")
     echo "$URL/$x"
