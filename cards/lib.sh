72ebe45e8593844d49efbd5ca715a64b1a5ed9a2
Jacques Supcik <jacques.supcik@hefr.ch>
2016-03-07 16:33:11 +0100

    Add caption (URL) to images

--- cards/lib.sh
+++ cards/lib.sh
@@ -5,7 +5,9 @@ page() {
     for i in $(seq 8); do
         x=$(../genid/genid -short -key "$SECRET")
         echo "$URL/$x" | qrencode -s 10 -o $temp/q.png
-        composite -geometry +700+346  $temp/q.png $SOURCE $temp/res$i.png
+        composite -geometry +720+320  $temp/q.png $SOURCE $temp/res$i.png
+        convert -font SourceCodePro -pointsize 22 -fill black -draw "text 50,810 \"$URL/$x\"" $temp/res$i.png $temp/resb$i.png
+        mv $temp/resb$i.png $temp/res$i.png
     done
     outfile=$(printf 'page-%03i.png' $pageno)
     echo $outfile
